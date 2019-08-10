916bc9d08a0c52069d97fcb5435f220ed5defb68
Oskar Mampe <oskarmampe@hotmail.co.uk>
2019-08-10 01:26:55 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from tree import *
+import operator
+
+test_point = ['vhigh', 'low', '3', '4', 'med', 'med']
+
+def classify(datapoint, tree):
+  if isinstance(tree, Leaf):
+    return max(tree.labels.items(), key=operator.itemgetter(1))[0]
+
+  answer = datapoint[tree.feature]
+  for branch in tree.branches:
+    if branch.value == answer:
+      return classify(datapoint, branch)
+
+print(classify(test_point, tree))
+print_tree(tree)
